55c00101e70035d1fbdb84c00ea5d725e8e8ba69
Ray <rayyiu66@gmail.com>
2021-02-11 17:57:32 -0800

    finished ISS 1

---
+++ iss.js
@@ -0,0 +1,20 @@
+//It will contain most of the logic for
+// fetching the data from each API endpoint.
+const request = require("request");
+const fetchMyIP = function (callback) {
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
